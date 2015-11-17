c206457e6b15a357bc05069c263e77ab83644009
Daniel Rugg <dfrugg@gmail.com>
2015-11-17 17:21:38 -0500

    Adding project files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to prop-shop
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/prop_shop/core_test.clj
@@ -0,0 +1,7 @@
+(ns prop-shop.core-test
+  (:require [clojure.test :refer :all]
+            [prop-shop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
