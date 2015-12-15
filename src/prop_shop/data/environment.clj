e1254f9bc84a2431efbd5e77ee15f0cb0e61e367
Justin Timbers <JMTimbers@gmail.com>
2015-12-15 18:52:07 -0500

    Added function for persisting Environment resources. Fixes #36

---
+++ src/prop_shop/data/environment.clj
@@ -0,0 +1,13 @@
+(ns prop-shop.data.environment
+  "Provides data access and manipulation for Environment resources."
+  {:author "Justin Timbers"}
+  (:require [prop-shop.data.base :as b]))
+
+
+(defn add-environment
+  "Persists a new Environment with the provided name."
+  {:added "0.1"}
+  [name]
+  (b/add-entity
+    {:name name
+     :type :environment}))
